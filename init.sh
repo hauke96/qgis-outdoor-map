50191ba78c293c6a2ab1793b1e21a2227f14a329
Hauke Stieler <mail@hauke-stieler.de>
2021-09-19 12:01:42 +0200

    Adjust init script to convert closed ST_LineString into polygons

--- init.sh
+++ init.sh
@@ -10,3 +10,9 @@ export PGUSER=postgres
 psql -c "CREATE EXTENSION hstore;" 2> /dev/stderr || echo "Skip hstore extension creation."
 
 osm2pgsql --create --slim -G --hstore --number-processes 4 "$1"
+
+# Convert closed line-strings for protection areas into polygons
+psql <<EOF
+INSERT INTO planet_osm_polygon SELECT * FROM planet_osm_line l WHERE ST_IsClosed(l.way) AND boundary='protected_area';
+UPDATE planet_osm_polygon SET way = ST_MakePolygon(way) WHERE ST_GeometryType(way)='ST_LineString';
+EOF
