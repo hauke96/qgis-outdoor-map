14910e33f163ad5fd52446bf0608e73045ad6b5e
Hauke Stieler <mail@hauke-stieler.de>
2023-09-19 10:27:55 +0200

    Add demo-website and generator script

---
+++ demo-website/create-demo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+OUTPUT="./dist"
+
+rm -rf $OUTPUT
+mkdir $OUTPUT
+
+cp -r ../tiles $OUTPUT
+
+cp ../style.json $OUTPUT/
+sed -i 's/http:\/\/localhost:8000/https:\/\/hauke-stieler.de\/public\/maps\/osm-outdoor-map-demo\/tiles/g' $OUTPUT/style.json
+
+cp index.html $OUTPUT/
+
+echo "Demo website prepared"
