d032701defc1c92a41319524662714fe209eae9c
Hauke Stieler <mail@hauke-stieler.de>
2023-09-20 00:14:43 +0200

    Adjust create-demo.sh script

--- demo-website/create-demo.sh
+++ demo-website/create-demo.sh
@@ -2,14 +2,25 @@
 
 OUTPUT="./dist"
 
+echo "Ensure output folder"
 rm -rf $OUTPUT
 mkdir $OUTPUT
 
+echo "Copy tiles"
 cp -r ../tiles $OUTPUT
 
+echo "Copy style"
 cp ../style.json $OUTPUT/
+
+echo "Adjust URL in style"
 sed -i 's/http:\/\/localhost:8000/https:\/\/hauke-stieler.de\/public\/maps\/osm-outdoor-map-demo/g' $OUTPUT/style.json
 
+echo "Copy index.html"
 cp index.html $OUTPUT/
+
+echo "Copy sprites"
+mkdir $OUTPUT/sprites
+cp ../sprites/sprite.json $OUTPUT/sprites
+cp ../sprites/sprite.png $OUTPUT/sprites
 
 echo "Demo website prepared"
