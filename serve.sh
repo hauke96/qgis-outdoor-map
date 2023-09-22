2699aea5ad522c95adc11b5a2416d2f2c062715b
Hauke Stieler <mail@hauke-stieler.de>
2023-09-22 10:50:36 +0200

    Use maputnik-cli in serve.sh

--- serve.sh
+++ serve.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+MAPUTNIK_PORT=8080
+TILES_PORT=8000
+
+# Start local maputnik on style
+echo "Start Maputnik on port $MAPUTNIK_PORT with local style"
+maputnik --port $MAPUTNIK_PORT --watch --file ./style.json &
+MAPUTNIK_PID=$!
+
 # npm i -g http-server
-http-server --cors -p 8000 .
+echo "Start local tile server"
+http-server --cors -p $TILES_PORT .
+
+echo "Exit Maputnik"
+kill $MAPUTNIK_PID
