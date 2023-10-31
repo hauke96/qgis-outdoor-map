5bb36c8f6f80c0b281a397613d52c577dd284e25
Hauke Stieler <mail@hauke-stieler.de>
2023-10-31 19:31:23 +0100

    Handle relations in preprocessor

--- preprocessor/main.go
+++ preprocessor/main.go
@@ -61,37 +61,9 @@ func main() {
 			inputNodes[obj.ID] = obj
 		case *osm.Way:
 			inputWays[obj.ID] = obj
-			centroidNode := handleWay(obj.Nodes, obj.Tags)
-			if centroidNode == nil {
-				continue
-			}
-			outputOsm.Append(centroidNode)
+			handleWay(obj, &outputOsm)
 		case *osm.Relation:
-			for _, member := range obj.Members {
-				if member.Type != osm.TypeWay {
-					continue
-				}
-
-				// TODO Merge lines to form potential polygons the relation might consist of. Like in this case: https://www.openstreetmap.org/relation/14931281
-
-				// Member ways do not contain their nodes here, only a ref-ID to the actual way
-				if memberWay, ok := inputWays[osm.WayID(member.Ref)]; ok {
-					tags := memberWay.Tags
-
-					// Sometimes, the actual ways do not have tags -> use the relation tags. If both have a value for
-					// same key, the general relation key is overridden by the specific way key.
-					if member.Role == "outer" {
-						tags = obj.Tags
-						tags = append(tags, memberWay.Tags...)
-					}
-
-					centroidNode := handleWay(memberWay.Nodes, tags)
-					if centroidNode == nil {
-						continue
-					}
-					outputOsm.Append(centroidNode)
-				}
-			}
+			handleRelation(obj, outputOsm)
 		}
 	}
 
@@ -109,23 +81,120 @@ func main() {
 	sigolo.Info("Done")
 }
 
-func handleWay(nodes osm.WayNodes, originalTags osm.Tags) *osm.Node {
+func handleRelation(relation *osm.Relation, outputOsm osm.OSM) {
+	// Collection of all ways with role "outer". Connected rings are later used to determine one centroid.
+	var outerRingWays []*osm.Way
+
+	for _, member := range relation.Members {
+		if member.Type != osm.TypeWay {
+			continue
+		}
+
+		// Member ways do not contain their nodes here, only a ref-ID to the actual way
+		if memberWay, ok := inputWays[osm.WayID(member.Ref)]; ok {
+			tags := memberWay.Tags
+
+			// Sometimes, the actual ways do not have tags -> use the relation tags. If both have a value for
+			// same key, the general relation key is overridden by the specific way key.
+			if member.Role == "outer" {
+				tags = relation.Tags
+				tags = append(tags, memberWay.Tags...)
+				outerRingWays = append(outerRingWays, memberWay)
+			}
+
+			//centroidNode := handleWay(memberWay.Nodes, tags)
+			//if centroidNode == nil {
+			//	continue
+			//}
+			//outputOsm.Append(centroidNode)
+		}
+	}
+
+	// Merge lines to form potential polygons of which the relation might consist of.
+	// Like in this case: https://www.openstreetmap.org/relation/14931281
+
+	// 1.) Collect all ways that form a ring (potentially in wrong order but that's not a problem). This is done by
+	// taking the last node of the current way A and find a way B that touches this endpoint. Now B=A and the process
+	// repeats until no further touching ways exist.
+	var ringWays [][]*osm.Way
+	for len(outerRingWays) != 0 {
+		// Start with any way, so we take the first one
+		currentOuterRingWay := outerRingWays[0]
+		currentLastNode := currentOuterRingWay.Nodes.NodeIDs()[len(currentOuterRingWay.Nodes)-1]
+
+		// Remove the way to now process it twice
+		outerRingWays = outerRingWays[1:]
+
+		// This array will store all ways that belong to the current ring we'll try to find.
+		waysOfCurrentRing := []*osm.Way{currentOuterRingWay}
+
+		// Find other way with "currentLastNode" as first or last node
+		for j := 0; j < len(outerRingWays); j++ {
+			otherRingWay := outerRingWays[j]
+
+			// Determine if and where the other way touches the current way
+			touchedFirstOfOther := currentLastNode == otherRingWay.Nodes.NodeIDs()[0]
+			touchedLastOfOther := currentLastNode == otherRingWay.Nodes.NodeIDs()[len(otherRingWay.Nodes)-1]
+
+			if touchedFirstOfOther || touchedLastOfOther {
+				// Update the current way variables. This means the "other" way is now our new "current" way.
+				currentOuterRingWay = otherRingWay
+				waysOfCurrentRing = append(waysOfCurrentRing, currentOuterRingWay)
+
+				// Store the opposite node on the way (touches the first node? Store the last. And vice versa)
+				if touchedFirstOfOther {
+					currentLastNode = currentOuterRingWay.Nodes.NodeIDs()[len(currentOuterRingWay.Nodes)-1]
+				} else if touchedLastOfOther {
+					currentLastNode = currentOuterRingWay.Nodes.NodeIDs()[0]
+				}
+
+				// Remove j-th way ("other way") and compensate the j++ of the loop due to the removed element
+				outerRingWays = append(outerRingWays[:j], outerRingWays[j+1:]...)
+				j--
+			}
+		}
+
+		ringWays = append(ringWays, waysOfCurrentRing)
+	}
+
+	// 2.) Create a centroid for each ring
+	for _, ring := range ringWays {
+		var allNodesOfRing []osm.WayNode
+		for _, way := range ring {
+			allNodesOfRing = append(allNodesOfRing, way.Nodes...)
+		}
+
+		// Do not use handleWays, since we have more like a point cloud here because we do not know (and care)
+		// about the order of the ways.
+		handleNodeCloud(allNodesOfRing, relation.Tags, &outputOsm)
+	}
+}
+
+// handleWay interprets the given nodes as one way. Its nodes are passed to handleNodeCloud and processed accordingly.
+func handleWay(way *osm.Way, outputOsm *osm.OSM) {
+	nodes := way.Nodes
 	// If way is not closed -> ignore, since it's not a polygon and not interesting for the current approach
 	if len(nodes) < 3 || nodes[0].ID != nodes[len(nodes)-1].ID {
-		return nil
+		// Given nodes do not form a polygon -> nothing to do here
+		return
 	}
-	// first node == last node -> Polygon
 
+	handleNodeCloud(nodes, way.Tags, outputOsm)
+}
+
+// handleNodeCloud processed the given nodes, determines a potential centroid and creates a new node if needed. This
+// node is added to the given OSM output.
+func handleNodeCloud(nodes osm.WayNodes, originalTags osm.Tags, outputOsm *osm.OSM) {
 	// Convert the nodes of the ways (which have NO GEOMETRY!) to a polygon with geometry.
 	var points []orb.Point
 	for _, wayNode := range nodes {
 		points = append(points, [2]float64{inputNodes[wayNode.ID].Lon, inputNodes[wayNode.ID].Lat})
 	}
-	polygon := orb.Polygon{points}
+	polygon := orb.MultiPoint(points)
 
 	centroid := determineCentroidFeatureFromOsmObject(polygon, originalTags)
 	if centroid == nil {
-		return nil
+		return
 	}
 
 	var tags []osm.Tag
@@ -147,7 +216,9 @@ func handleWay(nodes osm.WayNodes, originalTags osm.Tags) *osm.Node {
 	}
 	nodeIdCounter++
 
-	return node
+	if node != nil && node.Tags.Find("text") != "" {
+		outputOsm.Append(node)
+	}
 }
 
 func determineCentroidFeatureFromOsmObject(geometry orb.Geometry, tags osm.Tags) *geojson.Feature {
