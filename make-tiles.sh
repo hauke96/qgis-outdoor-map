3249720c54b2a88caa0c767826077298db7a80ae
Hauke Stieler <mail@hauke-stieler.de>
2023-12-09 21:23:15 +0100

    Adjust make-tile.sh script to also use preprocessor and PBF merging

--- make-tiles.sh
+++ make-tiles.sh
@@ -1,14 +1,55 @@
 #!/bin/bash
 
-OUTPUT="./tiles"
+set -e
+
+function log()
+{
+    echo "==> $1"
+}
+
+TMP=$(realpath ".tmp")
+OUTPUT=$(realpath "./tiles")
+SRC_DIR=$(pwd)
 
 if [ -z $1 ]
 then
-    echo "Missing input file."
+    log "ERROR: Missing input file."
     exit 1
 fi
 
+TMP_INPUT="$TMP/input.osm.pbf"
+TMP_CENTROIDS_OSM="$TMP/centroids.osm"
+TMP_CENTROIDS_PBF="$TMP/centroids.osm.pbf"
+TMP_DATA="$TMP/data.osm.pbf"
+
+log "Create empty temp-folder $TMP"
+rm -rf $TMP
+mkdir -p $TMP
+
+log "Create empty output folder $OUTPUT"
 rm -rf $OUTPUT
 mkdir -p $OUTPUT
 
-tilemaker --input $1 --output $OUTPUT --config ./tilemaker-config.json --process ./tilemaker-processing.lua
+# Prepare input data
+log "Copy intput data"
+cp $1 $TMP_INPUT
+
+# Run preprocessing script
+log "Run preprocessor in input data"
+cd preprocessor
+go run main.go -i "$TMP_INPUT" -o "$TMP_CENTROIDS_OSM"
+
+# Merge centroid to input data
+log "Turn preprocessed OSM-XML output into PBF"
+osmium cat "$TMP_CENTROIDS_OSM" -o "$TMP_CENTROIDS_PBF" --overwrite
+
+log "Merge preprocessed PBF into input data"
+osmium merge "$TMP_CENTROIDS_PBF" "$TMP_INPUT" -o "$TMP_DATA" --overwrite
+
+# Create tiles
+log "Go back to $SRC_DIR"
+cd "$SRC_DIR"
+log "Use tilemaker to create tiles from $TMP_DATA"
+tilemaker --input "$TMP_DATA" --output "$OUTPUT" --config ./tilemaker-config.json --process ./tilemaker-processing.lua
+
+log "DONE"
