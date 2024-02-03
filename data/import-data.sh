6baad5d71e8790fae9c3cc7c5aeb4e1c629cc0d5
Hauke Stieler <mail@hauke-stieler.de>
2024-02-03 10:22:25 +0100

    Add buildings to style

--- data/import-data.sh
+++ data/import-data.sh
@@ -217,7 +217,7 @@ esac
 echo "Processed region $1"
 
 echo "Filter $DATA by used tags into $DATA_FILTERED"
-osmium tags-filter -o $DATA_FILTERED $DATA nwr/aerialway,boundary,ele,highway,landuse,natural,railway,route,waterway,type,
+osmium tags-filter -o $DATA_FILTERED $DATA nwr/aerialway,boundary,building,ele,highway,landuse,natural,railway,route,waterway,type,
 
 echo "Move $DATA_FILTERED to general data folder as $DATA"
 cd ..
